2cf88c0aa6f38618d6a47314529049b446d151e7
Liran <liranamilov@gmail.com>
2022-03-15 14:01:42 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+from tkinter import *
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    root = Tk()
+
+
+    print('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
