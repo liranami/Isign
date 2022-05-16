f2ab66778d7306067887d17df461cd07d9b55502
liranami <58631201+liranami@users.noreply.github.com>
2022-05-17 01:50:26 +0300

    changed TO OOP * need fix video show

--- main.py
+++ main.py
@@ -1,41 +1,59 @@
 from tkinter import *
 from PIL import Image, ImageTk
-import userGUI, developerLogin
+import tkinter as tk
+from userGUI import UserGUI
+import developerLogin
 
-def main(win=None):
-    if win:
-        win.destroy()
-    window = Tk()
-    window.geometry("1024x600")
-    window.title("Isign")
-    icon = PhotoImage(file='assets/Logo.png')
-    window.iconphoto(True, icon)
-    window.configure(bg="#FFFFFF")
-    canvas = Canvas(window, bg="#FFFFFF", height=600, width=1024, bd=0, highlightthickness=0, relief="ridge")
-    canvas.place(x=0, y=0)
-    # create blue rectangle and on top of it our Isign LOGO
-    canvas.create_rectangle(0.0, 0.0, 500.0, 600.0, fill="#78BFDD", outline="")
-    logo_img = Image.open('assets/Logo.png')
-    logo_resize = logo_img.resize((450, 450), Image.ANTIALIAS)
-    logo_img = ImageTk.PhotoImage(logo_resize)
-    logo = canvas.create_image(240.0, 300.0, image=logo_img)
-    # end
-    # create Developer/User Btn - and texts
-    userBtn_image = PhotoImage(file='assets/user.png')
-    developerBtn_image = PhotoImage(file='assets/developer.png')
-    userBtn = Button(image=userBtn_image, borderwidth=0, highlightthickness=0,
-                     command=lambda: userGUI.changeToUserWin(window), relief="flat")
-    developerBtn = Button(image=developerBtn_image, borderwidth=0, highlightthickness=0,
-                          command=lambda: developerLogin.logIn(window), relief="flat")
-    userBtn.place(x=660, y=390, width=208, height=60)
-    developerBtn.place(x=660, y=250, width=208, height=60)
-    canvas.create_text(721, 191, anchor="nw", text="You are", fill="#000000", font=("Roboto", 24 * -1))
-    canvas.create_text(746, 340, anchor="nw", text="Or", fill="#000000", font=("Roboto", 24 * -1))
-    canvas.create_text(698, 56, anchor="nw", text="Isign", fill="#000000", font=("Roboto", 64 * -1))
-    # end
-    window.resizable(False, False)
-    window.mainloop()
+
+class MainApp(Tk):
+
+    def __init__(self):
+        super().__init__()
+        self.geometry("1024x600")
+        self.title("Isign")
+        self.icon = PhotoImage(file='assets/Logo.png')
+        self.iconphoto(True, self.icon)
+        self.configure(bg="#FFFFFF")
+        container = tk.Frame(self)
+        container.pack(side="top", fill="both", expand=True)
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
+        self.show_frame(Start(container, self))
+
+    def show_frame(self, main):
+        main.tkraise()
+
+
+class Start(Frame):
+
+    def __init__(self, parent, controller):
+        Frame.__init__(self, parent)
+        self.controller = controller
+
+        self.canvas = Canvas(parent, bg="#FFFFFF", height=600, width=1024, bd=0, highlightthickness=0, relief="ridge")
+        self.canvas.place(x=0, y=0)
+        # create blue rectangle and on top of it our Isign LOGO
+        self.canvas.create_rectangle(0.0, 0.0, 500.0, 600.0, fill="#78BFDD", outline="")
+        self.logo_img = Image.open('assets/Logo.png')
+        self.logo_resize = self.logo_img.resize((450, 450), Image.ANTIALIAS)
+        self.logo_img = ImageTk.PhotoImage(self.logo_resize)
+        self.canvas.create_image(240.0, 300.0, image=self.logo_img)
+        # end
+        # create Developer/User Btn - and texts
+        self.userBtn_image = PhotoImage(file='assets/user.png')
+        self.developerBtn_image = PhotoImage(file='assets/developer.png')
+        self.userBtn = Button(parent, image=self.userBtn_image, borderwidth=0, highlightthickness=0,
+                         command=lambda: controller.show_frame(UserGUI(parent, controller, self)), relief="flat")
+        self.developerBtn = Button(parent, image=self.developerBtn_image, borderwidth=0, highlightthickness=0,
+                              command=lambda: developerLogin.logIn(self), relief="flat")
+        self.userBtn.place(x=660, y=390, width=208, height=60)
+        self.developerBtn.place(x=660, y=250, width=208, height=60)
+        self.canvas.create_text(721, 191, anchor="nw", text="You are", fill="#000000", font=("Roboto", 24 * -1))
+        self.canvas.create_text(746, 340, anchor="nw", text="Or", fill="#000000", font=("Roboto", 24 * -1))
+        self.canvas.create_text(698, 56, anchor="nw", text="Isign", fill="#000000", font=("Roboto", 64 * -1))
 
 
 if __name__ == '__main__':
-    main()
+    gui = MainApp()
+    gui.resizable(False, False)
+    gui.mainloop()
--- userGUI.py
+++ userGUI.py
@@ -1,7 +1,6 @@
 from tkinter import *
 from tkinter import ttk
 from tkinter import messagebox
-import main
 import cv2
 import showVideoOnGui
 import tkinter
@@ -9,102 +8,93 @@ import PIL
 import threading
 import time
 from threading import Thread, Lock
-stop = False
+
+class UserGUI(Frame):
+
+    def __init__(self, parent, controller, main):
+        Frame.__init__(self, parent)
+        self.parent = parent
+        self.controller = controller
+        self.main = main
+        self.stop = False
+        self.canvas = Canvas(parent, bg="#FFFFFF", height=600, width=1024, bd=0, highlightthickness=0, relief="ridge")
+        self.canvas.place(x=0, y=0)
+        self.canvas.create_rectangle(578.0, 9.0, 578.0, 590.0, fill="#3D7892", outline="")
+        self.canvas.create_rectangle(26.0, 105.0, 551.0, 560.0, fill="#FFFFFF", outline="#000000")
+        self.canvas.create_text(620.0, 242.0, anchor="nw", text="translated words :", fill="#000000",
+                                font=("Roboto", 18 * -1))
+        self.canvas.create_text(702.0, 40.0, anchor="nw", text="Isign", fill="#000000", font=("Roboto", 64 * -1))
+        #### textarea   later : textarea.insert(tk.END, *variable*)
+        self.textarea = Text(parent, height=18, width=40)
+        self.textarea.place(x=615, y=270)
+
+        # Drop down
+        self.clicked = StringVar()
+        self.options = []
+        self.numOfCams = self.camerasConnected()
+        if self.numOfCams == 0:
+            self.messagebox.showerror("Error", "Please connect camera!")
+            self.clicked.set("no camera")
+        else:
+            for i in range(self.numOfCams):
+                self.options.append(i + 1)
+            # clicked.set(options[0])
+        self.chooseCamera = ttk.OptionMenu(parent, self.clicked, self.options[0], *self.options)
+        self.chooseCamera.place(x=680, y=150, width=100, height=30)
+        # create start/stop Btn - and texts
+        self.startBtn_image = PhotoImage(file='assets/start_btn.png')
+        self.stopBtn_image = PhotoImage(file='assets/stop_btn.png')
+        self.goBack_image = PhotoImage(file='assets/goBack.png')
+        self.startBtn = Button(parent, image=self.startBtn_image, borderwidth=0, highlightthickness=0,
+                               command=lambda: [threading.Thread(target=self.show_video).start(),
+                                                self.startvideo()], relief="flat")
+        self.stopBtn = Button(parent, image=self.stopBtn_image, borderwidth=0, highlightthickness=0,
+                              command=lambda: self.stopvideo(),
+                              relief="flat")
+        self.goBackBtn = Button(parent, image=self.goBack_image, borderwidth=0, highlightthickness=0,
+                                command=lambda: controller.show_frame(self.main), relief="flat")
+        self.startBtn.place(x=814, y=150, width=77, height=30)
+        self.stopBtn.place(x=902, y=150, width=77, height=30)
+        self.goBackBtn.place(x=26, y=32, width=25, height=25)
+
+    def startvideo(self):
+        self.stop = False
+
+    def stopvideo(self):
+        self.stop = True
+
+    def clearCapture(self, capture):
+        capture.release()
+        cv2.destroyAllWindows()
+
+    def camerasConnected(self):
+        n = 0
+        for i in range(3):
+            try:
+                cap = cv2.VideoCapture(i)
+                ret, frame = cap.read()
+                cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                self.clearCapture(cap)
+                n += 1
+            except:
+                self.clearCapture(cap)
+                break
+        return n
+
+    def show_video(self):
+        capture = cv2.VideoCapture(0)
+        ret, frame = capture.read()  # Read frame from webcam
+        canvas = tkinter.Canvas(self.parent, width=640, height=480)
+        canvas.place(x=10, y=80)
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        photo = PIL.ImageTk.PhotoImage(image=PIL.Image.fromarray(frame))
+        canvas.create_image(0, 0, image=photo, anchor=tkinter.NW)
+        if not self.stop:
+            self.parent.after(10, self.show_video())
 
 
-def startvideo():
-    global stop
-    stop = False
-
-
-def stopvideo():
-    global stop
-    stop = True
-    for i in range(10):
-        print("dsgdgfsdhshsfh")
-
-
-
-def clearCapture(capture):
-    capture.release()
-    cv2.destroyAllWindows()
-
-def camerasConnected():
-    n = 0
-    for i in range(3):
-        try:
-            cap = cv2.VideoCapture(i)
-            ret, frame = cap.read()
-            cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            clearCapture(cap)
-            n += 1
-        except:
-            clearCapture(cap)
-            break
-    return n
-
-def update(userGui):
-    global stop
-    capture = cv2.VideoCapture(0)
-    ret, frame = capture.read()  # Read frame from webcam
-    canvas = tkinter.Canvas(userGui, width=640, height=480)
-    canvas.place(x=10, y=80)
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    photo = PIL.ImageTk.PhotoImage(image=PIL.Image.fromarray(frame))
-    canvas.create_image(0, 0, image=photo, anchor=tkinter.NW)
-    if not stop:
-        userGui.after(1,update(userGui))
-    else:
-        return
-
-
-
-
-def changeToUserWin(oldWin):
-    oldWin.destroy()
-    userGui = Tk()
-    userGui.geometry("1024x600")
-    userGui.title("Isign - user")
-    userGui.configure(bg="#FFFFFF")
-    canvas = Canvas(userGui, bg="#FFFFFF", height=600, width=1024, bd=0, highlightthickness=0, relief="ridge")
-    canvas.place(x=0, y=0)
-    canvas.create_rectangle(578.0, 9.0, 578.0, 590.0, fill="#3D7892", outline="")
-    canvas.create_rectangle(26.0, 105.0, 551.0, 560.0, fill="#FFFFFF", outline="#000000")
-    canvas.create_text(620.0, 242.0, anchor="nw", text="translated words :", fill="#000000", font=("Roboto", 18 * -1))
-    canvas.create_text(702.0, 40.0, anchor="nw", text="Isign", fill="#000000", font=("Roboto", 64 * -1))
-    #### textarea   later : textarea.insert(tk.END, *variable*)
-    textarea = Text(userGui, height=18, width=40)
-    textarea.place(x=615, y=270)
-
-    # Drop down
-    clicked = StringVar()
-    options = []
-    numOfCams = camerasConnected()
-    if numOfCams == 0:
-        messagebox.showerror("Error", "Please connect camera!")
-        clicked.set("no camera")
-    else:
-        for i in range(numOfCams):
-            options.append(i + 1)
-        #clicked.set(options[0])
-    chooseCamera = ttk.OptionMenu(userGui, clicked, options[0], *options)
-    chooseCamera.place(x=680, y=150, width=100, height=30)
-    # create start/stop Btn - and texts
-    startBtn_image = PhotoImage(file='assets/start_btn.png')
-    stopBtn_image = PhotoImage(file='assets/stop_btn.png')
-    goBack_image = PhotoImage(file='assets/goBack.png')
-    startBtn = Button(image=startBtn_image, borderwidth=0, highlightthickness=0,
-                      command= lambda: [threading.Thread(target=update, args=(userGui,)).start(),startvideo()], relief="flat")
-    stopBtn = Button(image=stopBtn_image, borderwidth=0, highlightthickness=0, command = lambda: stopvideo(),
-                     relief="flat")
-    goBackBtn = Button(image=goBack_image, borderwidth=0, highlightthickness=0, command=lambda: main.main(userGui),
-                       relief="flat")
-    startBtn.place(x=814, y=150, width=77, height=30)
-    stopBtn.place(x=902, y=150, width=77, height=30)
-    goBackBtn.place(x=26, y=32, width=25, height=25)
-
-    #x = threading.Thread(target=update, args=(userGui,))
-    #update(userGui)
-    #x.start()
-    userGui.resizable(False, False)
-    userGui.mainloop()
+        # x = threading.Thread(target=update, args=(userGui,))
+        # update(userGui)
+        # x.start()
+        # userGui.resizable(False, False)
+        # userGui.mainloop()
